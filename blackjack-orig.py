b058a900ef41caf3a15bb3de54cb098b0ce53519
wiky-avis <peta-holding@yandex.ru>
2020-11-09 11:25:37 +0300

    Add project

--- blackjack-orig.py
+++ blackjack-orig.py
@@ -16,18 +16,18 @@ class BJ_Card(cards.Card): # расширяет функциональность
 
 class BJ_Deck(cards.Deck):
     """Колода  для  игры  в  "Блек-джек". """
-    def populate(self):
+    def populate(self): # переопределен метод cards.Deck's  populate(), и теперь новая колода класса BJ_Deck наполняется картами класса BJ_Card
         for suit in BJ_Card.SUITS:
             for rank in BJ_Card.RANKS:
                 self.cards.append(BJ_Card(rank, suit))
 
 
 class BJ_Hand(cards.Hand):
-    """Рука':  набор  карт  "Блек-джека"  у  одного  игрока."""
-    def __init__(self, name):
+    """Рука:  набор  карт  "Блек-джека"  у  одного  игрока."""
+    def __init__(self, name): # переопределен конструктор cards.Hand, добавив в него атрибут name, представляющий имя обладателя данной руки
         super(BJ_Hand, self).__init__()
         self.name = name
-    def __str__(self):
+    def __str__(self): # переопределен так что он теперь отображает сумму очков на руках у игрока
         rep = self.name + ':\t' + super(BJ_Hand, self).__str__()
         if self.total:
             rep += '(' + str(self.total) + ')'
@@ -54,38 +54,38 @@ class BJ_Hand(cards.Hand):
             # вошла  в  общую сумму
             t += 10
         return t
-    def is_busted(self):
+    def is_busted(self): # возвращает True, когда свойство total объекта принимает значение больше 21. В противном случае будет возвращено False.
         return self.total > 21
 
 class BJ_Player(BJ_Hand):
     """Игрок в Блек-Джек"""
-    def is_hitting(self):
+    def is_hitting(self): # возвращает True в том случае, если игроку угод­но получить еще карту, и False - если нет
         response = games.ask_yes_no('\n' + self.name + ', будете брать еще карты? (Y/N): ')
         return response == 'y'
-    def bust(self):
+    def bust(self): # объявляет, что участник перебрал
         print(self.name, 'перебрал.')
         self.lose()
-    def lose(self):
+    def lose(self): # объявляет, что участник проиграл
         print(self.name, 'проиграл.')
-    def win(self):
+    def win(self): # объявляет, что участник выиграл
         print(self.name, 'выиграл.')
-    def push(self):
+    def push(self): # объявляет ничью
         print(self.name, 'сыграл с компьютером в ничью.')
 
 
 class BJ_Dealer(BJ_Hand):
     """Дилер в игре Блек-Джек"""
-    def is_hitting(self):
-        return self.total < 17
-    def bust(self):
+    def is_hitting(self): # определяет, будет ли дилер брать дополнитель­ные карты
+        return self.total < 17 #  имея на руках не более 17 очков, обязан тянуть оче­редную карту, метод возвращает True,  если свойство tota l объекта не превыша­ет 17, а в противном случае возвращает False
+    def bust(self): # объявляет, что дилер перебрал
         print(self.name, 'перебрал')
-    def flip_first_card(self):
+    def flip_first_card(self): #  переворачивает первую карту дилера лицевой стороной вниз
         first_card = self.cards[0]
         first_card.flip()
         
-class BJ_Game(object):
+class BJ_Game(object): #  используется для создания объектов, которые будут представлять отдельные игры
     """Игра в Блек-Джек"""
-    def __init__(self, names):
+    def __init__(self, names): # Конструктор принимает список имен и создает на каждое имя по игроку. Кроме того, будут созданы дилер и колода
         self.players = []
         for name in names:
             player = BJ_Player(name)
@@ -95,14 +95,16 @@ class BJ_Game(object):
         self.deck.populate()
         self.deck.shuffle()
     @property
-    def still_playing(self):
+    def still_playing(self): # возвращает список игроков еще не перебравших в текущем раунде
         sp = []
         for player in self.players:
             if not player.is_busted():
                 sp.append(player)
         return sp
 
-    def __additional_cards(self, player):
+    def __additional_cards(self, player): # сдает игроку или дилеру дополнительные карты. Этот метод принимает в свой параметр 
+        # player объект класса BJ_Player или BJ_Dealer. До тех пор пока метод is_busted() данного объекта возвращает False,
+        # а метод is_hitting() возвращает True, программа будет сдавать карты.
         while not player.is_busted() and player.is_hitting():
             self.deck.deal([player])
             print(player)
@@ -114,7 +116,7 @@ class BJ_Game(object):
         self.deck.deal(self.players + [self.dealer], per_hand = 2)
         self.dealer.flip_first_card() #первая из карт, сданных диллеру, переворачивается рубашкой вверх
         for player in self.players:
-            print(player)
+            print(player) #  на экран выводятся <руки> всех участников игры
         print(self.dealer)
         # сдача дополнительных карт игрокам
         for player in self.players:
@@ -145,7 +147,8 @@ class BJ_Game(object):
             player.clear()
         self.dealer.clear()
 
-def main():
+def main(): # принимает имена всех игроков, организует их в виде списка и соз­дает объект BJ_ Game, 
+    # которому этот список передается как аргумент.
     print('\t\tДoбpo  пожаловать  за  игровой  стол  Блек-джека!\n')
     names = []
     number = games.ask_number('Cкoлькo  всего  игроков?  (1  - 7):  ', low = 1, hight = 8)
@@ -155,7 +158,8 @@ def main():
         print()
     game = BJ_Game(names)
     again = None
-    while again != 'n':
+    while again != 'n': # Затем функция вызывает метод рlау() данного объекта и будет делать это снова и 
+        # снова до тех пор, пока игроки не изъявят желание прекратить игру.
         game.play()
         again = games.ask_yes_no('\nXoтитe сыграть  еще  раз?  ')
 
